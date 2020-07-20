e26ed9e1e06386be7ace1d6d7bb50a2963419f04
hyy <1452626344@qq.com>
2020-07-20 14:20:13 +0800

    创建分支

--- helloGit/src/main/java/HelloGit.java
+++ helloGit/src/main/java/HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("this is git project");
         System.out.println("version 2");
+        System.out.println("创建一个分支");
 
     }
 }
